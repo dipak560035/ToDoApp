684899866dbaa38a8d784adb4309bf04f52d392d
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-03-12 21:58:14 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,4 @@
-// import { createBrowserRouter, Route, RouterProvider } from "react-router-dom"
-// import RootLayout from "./components/RootLayout"
-// import Categories from "./categories/Categories"
-// import Categoryitem from "./category-items/Categoryitem"
 
-
-
-// export default function App() {
-//     const router = createBrowserRouter([
-//         {
-//             path:'/',
-//             element:<RootLayout />,
-//             children:[
-//               {
-//                 index:true,
-//                 element:<Categories />
-//               },
-//               {
-//                 path:'category-items/:name',
-//                 element:<Categoryitem />
-//               }
-
-//             ]
-//         }
-//     ])
-//   return <RouterProvider router={router} />
-// // } 
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
