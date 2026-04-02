419960f652946e116ea09d2e24c6fe649a84333f
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-04-02 22:19:02 +0545

    Update hero.js

--- hero.js
+++ hero.js
@@ -1,4 +1,4 @@
 import { heroui } from "@heroui/react";
 // or import from theme package if you are using individual packages.
-// import { heroui } f
+// import { heroui } 
 export default heroui();
